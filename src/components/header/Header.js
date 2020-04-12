ef95c349f8dcb76bbddd0a55b76d377e18690f28
Praveen <prajagan@publicisgroupe.net>
2020-04-12 12:15:40 +0530

    changed the favicon and checking the auto CI CD build and deployment

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,9 +1,12 @@
 import React from 'react'
+import logo from '../../assets/favicon-32x32.png';
 
 function Header() {
     return (
         <div className='header'>
-            <div className='logo'>Y</div>
+            <div className='logo'>
+                <img src={logo} alt='Y' />
+            </div>
             <ul>
                 <li>top</li>
                 <li>new</li>
